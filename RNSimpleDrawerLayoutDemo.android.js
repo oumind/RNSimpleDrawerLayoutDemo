cc4c67a954045e828f641c983061c8ca797c3461
张衡 <zhangheng@zhanghengdeMacBook-Pro.local>
2016-05-27 16:20:18 +0800

    使用ToolbarAndroid和Icon.ToolbarAndroid控制抽屉导航

--- RNSimpleDrawerLayoutDemo.android.js
+++ RNSimpleDrawerLayoutDemo.android.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {
+  Alert,
   StyleSheet,
   Text,
   View,
@@ -8,21 +9,45 @@ import {
   TouchableHighlight,
   Image
 } from 'react-native';
+
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import logoImage from './assets/react_logo.png';
+import barsIcon from './assets/fa-bars.png';
+import ringIcon from './assets/ring.png';
 
 class RNSimpleDrawerLayoutDemo extends Component {
   constructor(props) {
     super(props);
+    this.imageToolbarActions = [
+      {title: '提醒', icon: ringIcon, show: 'always'},
+      {title: '夜间模式', show: 'never'},
+      {title: '设置选项', show: 'never'},
+    ];
+    this.iconToolbarActions = [
+      {title: '提醒', iconName: 'bell', show: 'always'},
+      {title: '夜间模式', show: 'never'},
+      {title: '设置选项', show: 'never'},
+    ];
 
     this.openDrawer = this.openDrawer.bind(this);
+    this.onImageActionSelected = this.onImageActionSelected.bind(this);
+    this.onIconActionSelected = this.onIconActionSelected.bind(this);
   }
 
   openDrawer() {
     this.drawer.openDrawer();
   }
+  onImageActionSelected(position) {
+    let action = this.imageToolbarActions[position];
+    Alert.alert(action.title, action.show);
+  }
+  onIconActionSelected(position) {
+    let action = this.iconToolbarActions[position];
+    Alert.alert(action.title, action.show);
+  }
 
   render() {
-    var navigationView = (
+    const navigationView = (
       <View style={{flex: 1, backgroundColor:'#FAFAFA'}}>
         <View style={styles.avatar}>
           <Image style={{width: 30, height: 30, backgroundColor: 'black'}} source={logoImage}/>
@@ -39,8 +64,26 @@ class RNSimpleDrawerLayoutDemo extends Component {
         drawerWidth={150}
         drawerPosition={DrawerLayoutAndroid.positions.left}
         renderNavigationView={() =>navigationView}>
-        <View style={{flex: 1, alignItems:'center'}}>
-          <Text style={{margin: 10, fontSize: 15, textAlign: 'right'}}>主页面</Text>
+        <View>
+          {/* 使用图片作图标 */}
+          <ToolbarAndroid
+            navIcon={barsIcon}
+            onIconClicked={this.openDrawer}
+            title="主页面-image"
+            titleColor="white"
+            actions={this.imageToolbarActions}
+            onActionSelected={this.onImageActionSelected}
+            style={styles.toolbar}/>
+
+          {/* 使用字体作图标 */}
+          <FontAwesome.ToolbarAndroid
+            navIconName="bars"
+            onIconClicked={this.openDrawer}
+            title="主页面-icon"
+            titleColor="white"
+            actions={this.iconToolbarActions}
+            onActionSelected={this.onIconActionSelected}
+            style={[styles.toolbar, {marginTop: 20}]}/>
 
           <TouchableHighlight onPress={this.openDrawer} style={styles.button}>
             <View style={[styles.inlineContainer, {alignSelf: 'center'}]}>
@@ -54,6 +97,10 @@ class RNSimpleDrawerLayoutDemo extends Component {
 }
 
 const styles = StyleSheet.create({
+  toolbar: {
+    backgroundColor: '#6DB82D',
+    height: 56,
+  },
   avatar : {
     flexDirection:'row',
     flexWrap:'wrap',
